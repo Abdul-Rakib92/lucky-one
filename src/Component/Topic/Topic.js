f37151df93daafe801bddd582a34b438f9bf09fb
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-03-26 04:30:54 +0600

    add to cart button added

--- src/Component/Topic/Topic.js
+++ src/Component/Topic/Topic.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './Topic.css';
 
 const Topic = (props) => {
-    const {id, picture, price, name} = props.course;
+    const {picture, price, name} = props.course;
     return (
         <div className='topic'>
             <img src={picture} alt="" />
+
+            <div>
+                <h4 className='topic-name'>{name}</h4>
+                <h4>Course Fee: ${price}</h4>
+            </div>
+
+           
+            <button className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+            </button>
+            
         </div>
+        
     );
 };
 
