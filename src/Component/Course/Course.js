20b13a89830b8f18088f7d5e8cca45f3d925ab7a
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-03-26 02:45:25 +0600

    usestate & useEffect added

--- src/Component/Course/Course.js
+++ src/Component/Course/Course.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Course.css';
 
 const Course = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect( () => {
+        fetch('courses.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    }, [])
+
+
     return (
         <div>
             <h1>Online Learning Course</h1>
             <div className='course-container'>
                 <div className="subjects-container">
-                    <h3>This is course</h3>
+                    <h3>This is course: {courses.length}</h3>
 
                 </div>
                 <div className="cart-container">
